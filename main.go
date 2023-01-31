0974d8cef5af90d7f54614a0f92d0422397a0d26
Salim Rashid <salimrashid1990@gmail.com>
2023-01-31 15:15:44 +0300

    Add functions for different endpoints

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ func main() {
 
 	http.HandleFunc("/", web_server.HomePage)
 
+	http.HandleFunc("/v1/nginx_install", web_server.NginxInstall)
+
+	http.HandleFunc("/v1/nginx_uninstall", web_server.NginxUninstall)
+
+	http.HandleFunc("/v1/nginx_update", web_server.NginxUpdate)
+
+	http.HandleFunc("/v1/nginx_start", web_server.NginxStart)
+
 	errs := web_server.Run(":8080", ":10443", map[string]string{
 		"cert": "./web_server/localhost.crt",
 		"key":  "./web_server/localhost.key",
--- web_server/web_server.go
+++ web_server/web_server.go
@@ -34,3 +34,19 @@ func Run(addr string, sslAddr string, ssl map[string]string) chan error {
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Edgeone API Server - https://edgeone.cloud/api")
 }
+
+func NginxInstall(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Do Nginx installation.")
+}
+
+func NginxUninstall(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Do Nginx uninstallation.")
+}
+
+func NginxUpdate(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Do Nginx update.")
+}
+
+func NginxStart(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Do Nginx restart.")
+}
