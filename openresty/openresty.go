a158291ff8e07bc0cb5313f6ef553e169f922652
Salim Rashid <salimrashid1990@gmail.com>
2023-02-05 11:39:01 +0300

    Add a function to detect Linux distribution and install Openresty

--- openresty/openresty.go
+++ openresty/openresty.go
@@ -1,9 +1,12 @@
 package openresty
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
 	"runtime"
+	"strings"
 )
 
 func OpenrestyConf() {
@@ -27,17 +30,54 @@ func OpenrestyInstall(w http.ResponseWriter, r *http.Request) {
 	case "darwin":
 		fmt.Println("MAC operating system")
 	case "linux":
-		fmt.Println("Linux")
+		// fmt.Println("Linux")
+		// Detect Linux distribution
+		DetectLinuxDist()
+		// Install Openresty
+		// InstallOpenresty()
+
 	default:
 		// fmt.Printf("%s.\n", os)
 		fmt.Printf("The operating system is not supported!")
 	}
 
-	// Detect Linux distribution
-
 	// Install Openresty
 
 	// Return a JSON for the installation status
+}
+
+func DetectLinuxDist() {
+	file, err := os.Open("/etc/os-release")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "NAME=") {
+			distro := strings.TrimPrefix(line, "NAME=")
+			distro = strings.Trim(distro, "\"")
+			// fmt.Println("Distribution:", distro)
+			// fmt.Println(distro)
+			InstallOpenresty(distro)
+			break
+		}
+	}
+}
+
+func InstallOpenresty(distro string) {
+	switch distro {
+	case "Ubuntu":
+		fmt.Printf("Install Openresty on %v.", distro)
+	case "CentOS":
+		fmt.Printf("Install Openresty on %v.", distro)
+	case "Amazon Linux":
+		fmt.Printf("Install Openresty on %v.", distro)
+	default:
+		fmt.Println("The operating system is not supported!.")
+	}
 }
 
 // func OpenrestyUninstall(w http.ResponseWriter, r *http.Request) {
